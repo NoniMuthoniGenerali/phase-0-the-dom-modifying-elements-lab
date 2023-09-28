b944859c61905d83b8506e1b6ab306b1dd6309b5
NoniMuthoni <noni.muthoni.noni@gmail.com>
2023-09-28 16:48:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+
+if (main) {
+    main.remove();
+};
+const newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "Noni is the champion"
